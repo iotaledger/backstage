93c02bd29dca53eaeb07c443d7128fe9007241bd
Louay Kamel <lk@overdb.com>
2021-09-16 14:54:26 -0400

    Add supervision unit-test

--- backstage-core/tests/supervision.rs
+++ backstage-core/tests/supervision.rs
@@ -3,51 +3,68 @@ use backstage::core::{
     Actor,
     ActorResult,
     EolEvent,
-    NullChannel,
     ReportEvent,
     Rt,
     Runtime,
     ScopeId,
     Service,
+    Shutdown,
     StreamExt,
     SupHandle,
 };
 
-// First actor
-struct First;
+struct Child;
 
-#[async_trait::async_trait]
-impl<S> Actor<S> for First
-where
-    S: SupHandle<Self>,
-{
-    type Data = ();
-    type Channel = NullChannel;
-    async fn init(&mut self, rt: &mut Rt<Self, S>) -> ActorResult<()> {
-        assert!(rt.service().is_initializing());
-        Ok(())
-    }
-    async fn run(&mut self, rt: &mut Rt<Self, S>, _data: Self::Data) -> ActorResult<()> {
-        assert!(rt.service().is_running());
-        Ok(())
+enum ChildEvent {
+    Microservice(ScopeId, Service),
+}
+
+impl<T> EolEvent<T> for ChildEvent {
+    fn eol_event(scope_id: ScopeId, service: Service, _actor: T, _r: ActorResult<()>) -> Self {
+        Self::Microservice(scope_id, service)
     }
 }
 
-struct Second;
+impl<T> ReportEvent<T> for ChildEvent {
+    fn report_event(scope_id: ScopeId, service: Service) -> Self {
+        Self::Microservice(scope_id, service)
+    }
+}
 
 #[async_trait::async_trait]
-impl<S> Actor<S> for Second
+impl<S> Actor<S> for Child
 where
     S: SupHandle<Self>,
 {
     type Data = ();
-    type Channel = NullChannel;
+    type Channel = AbortableUnboundedChannel<ChildEvent>;
     async fn init(&mut self, rt: &mut Rt<Self, S>) -> ActorResult<()> {
         assert!(rt.service().is_initializing());
+        // stops spawning children at the 11th level
+        if rt.depth() < 10 {
+            rt.spawn(None, Child).await?;
+            rt.spawn(None, Child).await?;
+        } else {
+            rt.handle().shutdown().await;
+        }
         Ok(())
     }
     async fn run(&mut self, rt: &mut Rt<Self, S>, _data: Self::Data) -> ActorResult<()> {
         assert!(rt.service().is_running());
+        while let Some(event) = rt.inbox_mut().next().await {
+            match event {
+                ChildEvent::Microservice(scope_id, service) => {
+                    if service.is_stopped() {
+                        rt.remove_microservice(scope_id);
+                        if rt.microservices_stopped() {
+                            rt.inbox_mut().close();
+                        }
+                    } else {
+                        rt.upsert_microservice(scope_id, service);
+                    }
+                }
+            }
+        }
         Ok(())
     }
 }
@@ -79,11 +96,10 @@ where
     type Data = ();
     type Channel = AbortableUnboundedChannel<RuntimeEvent>;
     async fn init(&mut self, rt: &mut Rt<Self, S>) -> ActorResult<()> {
-        // start short lived 2000 actors
-        for n in 0..1000 {
-            rt.spawn(format!("f{}", n), First).await?;
-            rt.spawn(format!("s{}", n), Second).await?;
-        }
+        assert!(rt.depth() == 0);
+        rt.spawn(None, Child).await?;
+        rt.spawn(None, Child).await?;
+        assert!(rt.service().microservices().len() == 2);
         Ok(())
     }
     async fn run(&mut self, rt: &mut Rt<Self, S>, _data: Self::Data) -> ActorResult<()> {
@@ -91,7 +107,7 @@ where
         while let Some(event) = rt.inbox_mut().next().await {
             match event {
                 RuntimeEvent::Microservice(scope_id, service) => {
-                    assert!(service.is_type::<First>() || service.is_type::<Second>());
+                    assert!(service.is_type::<Child>());
                     if service.is_stopped() {
                         rt.remove_microservice(scope_id);
                     } else {
@@ -109,7 +125,7 @@ where
 }
 
 #[tokio::test(flavor = "multi_thread")]
-async fn actors() {
+async fn supervision() {
     let runtime = Runtime::new(None, RuntimeActor).await.expect("Runtime to run");
     runtime.block_on().await.expect("Runtime to gracefully shutdown");
 }
